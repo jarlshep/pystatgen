180c317eac7b175585a5c939e47dd272b93abafc
jarlshep <239417045+jarlshep@users.noreply.github.com>
2026-03-04 23:29:05 +0100

    Leaf node and tests created

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,22 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+        
+        if self.children:
+            raise Exception("LeafNodes cannot have children")
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        elif self.tag is None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, props: {self.props})'
